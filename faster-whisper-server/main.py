825957ce1910f961d2d6a1180a786485bc7a87b9
charleslee0212 <charleslee0212@gmail.com>
2024-09-02 01:06:53 -0600

    Live Transcription

--- faster-whisper-server/main.py
+++ faster-whisper-server/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, WebSocket, HTTPException, UploadFile, File
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, HTTPException, UploadFile, File
 from faster_whisper import WhisperModel
 from faster_whisper.vad import VadOptions, get_speech_timestamps
 from io import BytesIO
@@ -93,7 +93,6 @@ def is_speaking(data):
 
 
 CHUNK_SIZE = get_chunk_size(1, 16000)
-MAX_DURATION = get_chunk_size(10, 16000)
 
 @app.websocket("/live-transcription")
 async def transcribe(websocket: WebSocket):
@@ -101,66 +100,82 @@ async def transcribe(websocket: WebSocket):
 
     # Initialize variables for chunk-based processing
     buffer = BytesIO()
+    audio = BytesIO()
 
     # Keep track of processed chunks
     processed = 0
 
-    while True:
+    try:
+        while True:
 
-        # Receive audio data in chunks
-        data = await websocket.receive_bytes()
+            # Receive audio data in chunks
+            data = await websocket.receive_bytes()
 
-        buffer.seek(0, 2)
-        buffer.write(data)
-        buffer.seek(0)
+            buffer.seek(0, 2)
+            buffer.write(data)
+            buffer.seek(0)
 
-        byte_data = buffer.getvalue()
-        byte_len = len(byte_data)
+            byte_data = buffer.getvalue()
+            byte_len = len(byte_data)
 
-        # Process complete chunks of audio data
-        if byte_len // CHUNK_SIZE > processed:
+            # Process complete chunks of audio data
+            if byte_len >= CHUNK_SIZE:
 
-            # VAD
-            float_arry = np.frombuffer(byte_data[int(processed * CHUNK_SIZE):], dtype=np.float32)
-            speaking = is_speaking(float_arry)
-            if not speaking:
-                processed += 1
-                continue
-
-            # Convert the chunk to PCM if necessary
-            pcm_data = np.frombuffer(byte_data, dtype=np.float32)
-
-            # Transcribe the audio chunk
-            segments, info = model.transcribe(pcm_data)
-
-            # Reset buffer at MAX_DURATION
-            finilize = False
-            if byte_len > MAX_DURATION:
+                # VAD
+                float_arry = np.frombuffer(byte_data, dtype=np.float32)
+                speaking = is_speaking(float_arry)
                 buffer.seek(0)
                 buffer.truncate(0)
-                processed = 0
-                finilize = True
+                finilize = False
+                if not speaking:
+                    if len(audio.getvalue()) > 0:
+                        finilize = True
+                    else:
+                        continue
 
-            # Send transcription results
-            partition = []
-            transcription = []
+                audio.seek(0, 2)
+                audio.write(byte_data)
+                audio.seek(0)
+                
+                audio_data = audio.getvalue()
+                audio_len = len(audio_data)
 
-            for segment in segments:
-                partition.append(
-                    {
-                        "start": segment.start,
-                        "end": segment.end,
-                        "text": segment.text.lstrip(),
-                        "info": {
-                            "language": info.language,
-                            "probability": info.language_probability,
-                        },
-                    }
-                )
-                transcription.append(segment.text.lstrip())
+                if finilize:
+                    audio.seek(0)
+                    audio.truncate(0)
+                    processed = 0
 
-            await websocket.send_json({"partition": partition, "transcription": " ".join(transcription), "type": "final" if finilize else "partial"})
-            processed += 1
+                if audio_len // CHUNK_SIZE > processed:
+                    # Convert the chunk to PCM
+                    pcm_data = np.frombuffer(audio_data, dtype=np.float32)
+
+                    # Transcribe the audio chunk
+                    segments, info = model.transcribe(audio=pcm_data, condition_on_previous_text=False)
+
+                    # Send transcription results
+                    partition = []
+                    transcription = []
+
+                    for segment in segments:
+                        partition.append(
+                            {
+                                "start": segment.start,
+                                "end": segment.end,
+                                "text": segment.text.lstrip(),
+                                "info": {
+                                    "language": info.language,
+                                    "probability": info.language_probability,
+                                },
+                            }
+                        )
+                        transcription.append(segment.text.lstrip())
+
+                    await websocket.send_json({"partition": partition, "transcription": " ".join(transcription), "type": "final" if finilize else "partial"})
+                    processed += 1
+    except TimeoutError:
+        print("Timeout! No data was detected!")
+    except WebSocketDisconnect as e:
+        print(f"Client disconnected: {e}")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, ssl_keyfile='./SSL/key.pem', ssl_certfile='./SSL/cert.pem')
